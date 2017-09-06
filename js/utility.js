55932e3c52ebefaedee19eff8ec1ed5b78782f44
Kin Lane <info@apievangelist.com>
2017-09-05 23:20:28 -0400

    update

--- js/utility.js
+++ js/utility.js
@@ -128,3 +128,14 @@ function getUrlVar(key){
     var result = new RegExp(key + "=([^&]*)", "i").exec(window.location.search);
     return result && unescape(result[1]) || "";
 }
+
+function titleCase(str) {  
+  str = str.toLowerCase().split(' ');
+
+  for(var i = 0; i < str.length; i++){
+    str[i] = str[i].split('');
+    str[i][0] = str[i][0].toUpperCase();
+    str[i] = str[i].join('');
+  }
+  return str.join(' ');
+}
